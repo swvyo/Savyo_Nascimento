e86fda16d0e3f26d84f994ad4598caf010dc249c
Savyo Nascimento <37352946+swvyo@users.noreply.github.com>
2018-06-29 01:03:39 -0300

    Create carros_joao_maria.py
    Questões Teste Inicial

---
+++ teste_inicial/carros_joao_maria.py
@@ -0,0 +1,15 @@
+continuar = input()
+veloc = []
+ano = []
+
+if continuar != 'n' and continuar != 'N':
+    while continuar != 'n' and continuar != 'N':
+        ano.append(int(input()))
+        veloc.append(float(input()))
+        continuar = input()
+    print(max(veloc))
+    print(max(ano))
+    print(sum(veloc) / len(veloc))
+    
+else:
+    print("ZERO")
